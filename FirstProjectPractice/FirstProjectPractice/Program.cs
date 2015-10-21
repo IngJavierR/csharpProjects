a5cddad1032bcc1f969da4e55c64d268c3fd63d4
IngJavier <hazelapd@gmail.com>
2015-10-20 23:17:46 -0500

    Se agrega gitignore

--- FirstProjectPractice/FirstProjectPractice/Program.cs
+++ FirstProjectPractice/FirstProjectPractice/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstProjectPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Prueba");
+            Console.ReadLine();
         }
     }
 }
