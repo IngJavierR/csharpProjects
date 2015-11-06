1c7db4c895192daf6de7cc47b9f50d88136a7bc2
IngJavier <hazelapd@gmail.com>
2015-11-05 23:38:19 -0600

    Se agrega Custom Exception

---
+++ ExceptionManage/ExceptionManage/MizuhoException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionManage
+{
+    public class MizuhoException : Exception
+    {
+        public int Code;
+        public MizuhoException()
+        {
+        }
+
+        public MizuhoException(string message, int code) : base(message)
+        {
+            Code = code;
+        }
+
+        public MizuhoException(string message, int code, Exception innerException) : base(message, innerException)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ ExceptionManage/ExceptionManage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+namespace ExceptionManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Start");
+                throw new MizuhoException("Error general", 101);
+
+            }
+            catch (MizuhoException ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Code: " + ex.Code);
+                Console.WriteLine("Inner: " + ex.InnerException);
+            }
+
+            try
+            {
+                Console.WriteLine("Start2");
+                throw new Exception("Error general Exc");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Inner: " + ex.InnerException);
+            }
+            Console.Read();
+        }
+    }
+}
