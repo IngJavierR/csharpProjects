7f0fef139a27144391a7466b1c0f9b9f080d1cd8
IngJavier <hazelapd@gmail.com>
2016-01-10 23:20:06 -0600

    Se agrega mvvmlight

---
+++ MvvmLigthTestWpf/MvvmLigthTestWpf/ViewModel/CaptureViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MvvmLigthTestWpf.ViewModel
+{
+    class CaptureViewModel : ViewModelBase
+    {
+        public CaptureViewModel()
+        {
+
+        }
+
+        #region Command
+
+        private RelayCommand showMessageCommand;
+        public RelayCommand ShowMessageCommand
+        {
+            get { return showMessageCommand ?? (showMessageCommand = new RelayCommand(ShowMessage, () => true)); }
+        }
+        
+        #endregion
+
+        #region Methods
+        private void ShowMessage()
+        {
+            MessageBox.Show("Prueba2");
+        }
+        #endregion Methods
+    }
+}
