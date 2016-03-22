bafce5633e7bd2279292f06b768383331fd1e6ee
IngJavier <hazelapd@gmail.com>
2016-03-21 18:07:18 -0600

    Se agrega SQLlite

---
+++ SQLiteAdmin/SQLiteAdmin/Db/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Linq.Mapping;
+
+namespace SQLiteAdmin.Db.Entities
+{
+    [Table(Name = "employee")]
+    public class Employee
+    {
+        [Column(Name = "empid", IsPrimaryKey = true, IsDbGenerated = false)]
+        public int Empid { get; set; }
+        [Column(Name = "name")]
+        public string Name { get; set; }
+        [Column(Name = "title")]
+        public string Title { get; set; }
+    }
+}
